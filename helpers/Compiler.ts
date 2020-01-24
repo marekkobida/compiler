6684aab4a914c099f64d64d0e55ed918d73374dc
marekkobida <marek.kobida@gmail.com>
2020-01-24 10:36:54 +0100

    First

---
+++ helpers/Compiler.ts
@@ -0,0 +1,89 @@
+import path from 'path';
+
+import * as helpers from '@redred/helpers/server';
+import * as json from '@redred/pages/private/types/json';
+import * as t from 'io-ts';
+import Container from '@redred/pages/private/Container';
+
+class Compiler {
+  containers: Map<string, Container> = new Map();
+
+  addContainer (container: t.TypeOf<typeof json.Compiler>['containers'][0]): Container {
+    const addedContainer = new Container(null, null, []);
+
+    for (let i = 0; i < container.inputs.length; i += 1) {
+      const input = container.inputs[i];
+
+      addedContainer.inputs.set(input, null);
+    }
+
+    addedContainer.path = container.path;
+    addedContainer.version = container.version;
+
+    this.containers.set(addedContainer.path, addedContainer);
+
+    return addedContainer;
+  }
+
+  afterCompilation (container: Container) {
+    if (container.path) {
+      let isCompiled = true;
+
+      container.inputs.forEach((input) => (input === null ? isCompiled = false : null));
+
+      if (isCompiled) {
+        try {
+          const $ = path.resolve(container.path, './public/assets/server.js');
+
+          delete __non_webpack_require__.cache[__non_webpack_require__.resolve($)];
+
+          const $$: Container = __non_webpack_require__($).default;
+
+          container.error = null;
+          container.id = $$.id;
+          container.name = $$.name;
+          container.pages = $$.pages;
+
+          container.pages.forEach((page) => {
+            try {
+              page.context = { ...page.context, container, };
+
+              page.toHTML();
+
+              if (typeof page.html === 'string') {
+                helpers.write(`${container.path}/public/${page.name}.html`, page.html);
+              }
+            } catch (error) {
+              container.error = error.stack;
+            }
+
+            delete page.context.container;
+          });
+        } catch (error) {
+          container.error = error.stack;
+        }
+
+        this.containersToJSON(this.containers);
+
+        container.inputs.forEach((input, inputPath) => container.inputs.set(inputPath, null));
+      }
+    }
+  }
+
+  containersToJSON (containers: Compiler['containers']) {
+    const compiled: t.TypeOf<typeof json.Compiled> = { containers: [], };
+
+    containers.forEach((container) => {
+      if (container.path && container.version) {
+        compiled.containers = [
+          ...compiled.containers,
+          container.toJSON(),
+        ];
+      }
+    });
+
+    helpers.write('./compiled.json', JSON.stringify(compiled, null, 2));
+  }
+}
+
+export default Compiler;
---
+++ index.ts
@@ -0,0 +1,170 @@
+import fs from 'fs';
+import http from 'http';
+
+import * as helpers from '@redred/helpers/server';
+import * as json from '@redred/pages/private/types/json';
+
+import Compiler from './helpers/Compiler';
+import compile from './helpers/compile';
+
+const compiler = new Compiler();
+
+let S: { date: number, message: string, type: string, }[] = [];
+
+function test (message: any, type: 'error' | 'information' | 'warning') {
+  S = [
+    {
+      date: +new Date(), message: JSON.stringify(message, null, 2), type,
+    },
+    ...S,
+  ];
+}
+
+const server = http.createServer(async (request, response) => {
+  const url = new URL(`file://${request.url}`);
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  response.statusCode = 200;
+
+  try {
+    test(`http://127.0.0.1:1337${url.pathname}${url.search}`, 'information');
+
+    const compiledJSON = await helpers.validateInputFromPath(json.Compiled, './compiled.json');
+    const compilerJSON = await helpers.validateInputFromPath(json.Compiler, './compiler.json');
+
+    if (url.pathname === '/test') {
+      response.end(JSON.stringify(S));
+
+      return;
+    }
+
+    if (url.pathname === '/compile') {
+      if (url.searchParams.has('path')) {
+        for (let i = 0; i < compilerJSON.containers.length; i += 1) {
+          const container = compilerJSON.containers[i];
+
+          if (container.path === url.searchParams.get('path')) {
+            if (url.searchParams.has('version')) {
+              const version = url.searchParams.get('version');
+
+              if (version === 'development' || version === 'production') {
+                container.version = version;
+              }
+            }
+
+            if (compiler.containers.has(container.path)) {
+              response.end(JSON.stringify(`The path "${container.path}" exists in the compiler.`));
+
+              test(`The path "${container.path}" exists in the compiler.`, 'warning');
+
+              return;
+            }
+
+            compiler.addContainer(container);
+
+            compile(compiler);
+
+            response.end(JSON.stringify(`The path "${container.path}" was added to the compiler.`));
+
+            test(`The path "${container.path}" was added to the compiler.`, 'information');
+
+            return;
+          }
+        }
+      }
+    }
+
+    if (url.pathname === '/compiled.json') {
+      response.end(JSON.stringify(compiledJSON));
+
+      test(compiledJSON, 'information');
+
+      return;
+    }
+
+    if (url.pathname === '/compiler.json') {
+      response.end(JSON.stringify(compilerJSON));
+
+      test(compilerJSON, 'information');
+
+      return;
+    }
+
+    for (let i = 0; i < compiledJSON.containers.length; i += 1) {
+      const container = compiledJSON.containers[i];
+
+      const _ = container.path.replace(/^\.\//, '');
+      const __ = new RegExp(`\\/${_}`).exec(url.pathname);
+      const ___ = new RegExp(`\\/${_}\\/public\\/(.+)`).exec(url.pathname);
+
+      if (__) {
+        if (container.error) {
+          throw new Error(container.error);
+        }
+      }
+
+      if (___) {
+        if (container.error) {
+          throw new Error(container.error);
+        }
+
+        const readableStream = fs.createReadStream(`${container.path}/public/${___[1]}`);
+
+        let data: Array<Buffer> = [];
+
+        readableStream.on('data', ($) => {
+          data = [ ...data, $, ];
+        });
+
+        readableStream.on('end', () => {
+          const $ = /([^.]+)$/.exec(url.pathname);
+
+          switch ($ ? $[1] : '') {
+            case 'css':
+              response.setHeader('Content-Type', 'text/css');
+              break;
+            case 'html':
+              response.setHeader('Content-Type', 'text/html; charset=utf-8');
+              break;
+            case 'js':
+              response.setHeader('Content-Type', 'application/javascript');
+              break;
+            case 'map':
+              response.setHeader('Content-Type', 'application/json');
+              break;
+            case 'otf':
+              response.setHeader('Content-Type', 'font/otf');
+              break;
+            case 'png':
+              response.setHeader('Content-Type', 'image/png');
+              break;
+            default:
+              response.setHeader('Content-Type', 'text/plain');
+          }
+
+          response.end(Buffer.concat(data));
+        });
+
+        readableStream.on('error', async () => {
+          response.end(JSON.stringify('The request is not valid.'));
+
+          test('The request is not valid.', 'error');
+        });
+
+        return;
+      }
+    }
+
+    throw new Error('The request is not valid.');
+  } catch (error) {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    response.end(JSON.stringify(error.stack));
+
+    test(error.stack, 'error');
+  }
+});
+
+server.listen(1337);
