d34df760a669852204dbd91caba483731d2aeefa
Marek Kobida <marek.kobida@gmail.com>
2020-06-02 22:47:12 +0200

    updated compiler (test)
    zmenilo sa "bytes" -> "size" pri PAGES

---
+++ private/CompiledContainer.ts
@@ -0,0 +1,41 @@
+import * as t from 'io-ts';
+import Container from '@redredsk/pages/private/Container';
+import path from 'path';
+import writeFile from '@redredsk/helpers/private/writeFile';
+import { CompilerInputFilePackage, } from '@redredsk/compiler/private/types/CompilerInputFile';
+import { CompilerOutputFilePackage, } from '@redredsk/compiler/private/types/CompilerOutputFile';
+
+class CompiledContainer {
+  constructor (inputFilePackage: t.TypeOf<typeof CompilerInputFilePackage>, outputFilePackage: t.TypeOf<typeof CompilerOutputFilePackage>) {
+    try {
+      const $ = path.resolve(inputFilePackage.path, 'public/server.js');
+
+      delete __non_webpack_require__.cache[__non_webpack_require__.resolve($)];
+
+      const compiledContainer: Container = __non_webpack_require__($).default;
+
+      for (let i = 0; i < compiledContainer.pages.length; i += 1) {
+        const compiledContainerPage = compiledContainer.pages[i];
+
+        compiledContainerPage.context = {
+          ...compiledContainerPage.context,
+          compiledContainer,
+          inputFilePackage,
+          outputFilePackage,
+        };
+
+        compiledContainerPage.toHTML();
+
+        if (typeof compiledContainerPage.html === 'string') {
+          writeFile(`${inputFilePackage.path}/public/${compiledContainerPage.name}.html`, compiledContainerPage.html);
+        }
+      }
+
+      outputFilePackage.compiledContainer = compiledContainer.toJSON();
+    } catch (error) {
+      console.log([ error.message, error.stack, ]);
+    }
+  }
+}
+
+export default CompiledContainer;
--- private/Compiler.ts
+++ private/Compiler.ts
@@ -1,11 +1,9 @@
 import * as t from 'io-ts';
-import Container from '@redredsk/pages/private/Container';
+import CompiledContainer from './CompiledContainer';
 import InputFile from './InputFile';
 import OutputFile from './OutputFile';
 import path from 'path';
-import writeFile from '@redredsk/helpers/private/writeFile';
 import { CompilerInputFilePackage, } from '@redredsk/compiler/private/types/CompilerInputFile';
-import { CompilerOutputFilePackage, } from '@redredsk/compiler/private/types/CompilerOutputFile';
 
 process.chdir(process.mainModule.path);
 
@@ -23,37 +21,6 @@ class Compiler {
     this.outputFile.writeFile({ packages: [], });
 
     this.compile('./packages/compiler');
-  }
-
-  private afterCompilation (inputFilePackage: t.TypeOf<typeof CompilerInputFilePackage>, outputFilePackage: t.TypeOf<typeof CompilerOutputFilePackage>) {
-    try {
-      const $ = path.resolve(inputFilePackage.path, 'public/server.js');
-
-      delete __non_webpack_require__.cache[__non_webpack_require__.resolve($)];
-
-      const compiledContainer: Container = __non_webpack_require__($).default;
-
-      for (let i = 0; i < compiledContainer.pages.length; i += 1) {
-        const compiledContainerPage = compiledContainer.pages[i];
-
-        compiledContainerPage.context = {
-          ...compiledContainerPage.context,
-          compiledContainer,
-          inputFilePackage,
-          outputFilePackage,
-        };
-
-        compiledContainerPage.toHTML();
-
-        if (typeof compiledContainerPage.html === 'string') {
-          writeFile(`${inputFilePackage.path}/public/${compiledContainerPage.name}.html`, compiledContainerPage.html);
-        }
-      }
-
-      outputFilePackage.compiledContainer = compiledContainer.toJSON();
-    } catch (error) {
-      console.log([ error.message, error.stack, ]);
-    }
   }
 
   async compile (path: t.TypeOf<typeof CompilerInputFilePackage>['path']) {
@@ -98,7 +65,7 @@ class Compiler {
               outputFilePackage.compiledFiles = [ ...outputFilePackage.compiledFiles, { ...right.toJson(), path: packageFileToCompile.path, }, ];
 
               if (inputFilePackage.filesToCompile.length === outputFilePackage.compiledFiles.length) {
-                this.afterCompilation(inputFilePackage, outputFilePackage);
+                new CompiledContainer(inputFilePackage, outputFilePackage);
 
                 this.outputFile.writeFile(outputFile);
 
