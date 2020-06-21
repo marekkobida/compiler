317869cb61223091ba4a6474f10f75dca50fdbad
Marek Kobida <marek.kobida@gmail.com>
2020-06-21 23:36:44 +0200

    updated

--- private/CompiledContainer.ts
+++ private/CompiledContainer.ts
@@ -136,11 +136,11 @@ class CompiledContainer {
         }
 
         for (const assetName in compilation.assets) {
-          if (/\.css|\.js/.test(assetName)) {
+          if (/(\.css|\.js)$/.test(assetName)) {
             compilation.assets[assetName] = new ConcatSource(Buffer.from('2f2a2120436f707972696768742032303230204d6172656b204b6f62696461202a2f', 'hex').toString('utf-8'), '\n', compilation.assets[assetName]);
           }
 
-          if (/\.html/.test(assetName)) {
+          if (/\.html$/.test(assetName)) {
             compilation.assets[assetName] = new ConcatSource(Buffer.from('3c212d2d20436f707972696768742032303230204d6172656b204b6f62696461202d2d3e', 'hex').toString('utf-8'), '\n', compilation.assets[assetName]);
           }
         }
