1e391a3112670d315a6c575e3f96dec762ada191
Marek Kobida <marek.kobida@gmail.com>
2020-06-22 21:26:54 +0200

    moved statistics to compiler

--- private/Compiler/index.ts
+++ private/Compiler/index.ts
@@ -1,8 +1,7 @@
 import Compiler from './Compiler';
+import StatisticsFile from './StatisticsFile';
 import http from 'http';
 import mime from '@redredsk/helpers/private/mime';
-import os from 'os';
-import p from './package.json';
 import path from 'path';
 import readFile from '@redredsk/helpers/private/readFile';
 import test from './test';
@@ -15,63 +14,96 @@ if (l < r) {
 
   const compiler = new Compiler();
 
-  const server = http.createServer(async (request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  const statisticsFile = new StatisticsFile();
 
-    try {
-      response.statusCode = 200;
+  function createServer (name: string, version: string): void {
+    const createdServer = http.createServer(async (request, response) => {
+      response.setHeader('Access-Control-Allow-Origin', '*');
+      response.setHeader('Content-Type', 'application/json; charset=utf-8');
 
-      const requestedURL = new URL(request.url, `http://${request.headers.host}`);
-      const requestedURLParameters = requestedURL.searchParams;
+      try {
+        response.statusCode = 200;
 
-      if (requestedURL.pathname === '/about.json') {
-        response.end(JSON.stringify({ name: os.hostname(), version: p.version, }));
+        const requestedURL = new URL(request.url, `http://${request.headers.host}`);
+        const requestedURLParameters = requestedURL.searchParams;
 
-        return;
-      }
-
-      const isCompilerCompileFunctionRequested = requestedURL.pathname === '/compiler/compile';
-      const isCompilerInputFileRequested = requestedURL.pathname === `/compiler/${compiler.inputFile.fileName}`;
-      const isCompilerOutputFileRequested = requestedURL.pathname === `/compiler/${compiler.outputFile.fileName}`;
-
-      if (isCompilerCompileFunctionRequested) {
-        const pathFromRequestedURLParameters = requestedURLParameters.get('path');
-
-        if (pathFromRequestedURLParameters) {
-          await compiler.compile(pathFromRequestedURLParameters);
-
-          response.end();
+        if (requestedURL.pathname === '/about.json') {
+          response.end(JSON.stringify({ name, version, }));
 
           return;
         }
+
+        // Compiler
+
+        const isCompilerCompileFunctionRequested = requestedURL.pathname === '/compiler/compile';
+        const isCompilerInputFileRequested = requestedURL.pathname === `/compiler/${compiler.inputFile.fileName}`;
+        const isCompilerOutputFileRequested = requestedURL.pathname === `/compiler/${compiler.outputFile.fileName}`;
+
+        if (isCompilerCompileFunctionRequested) {
+          const pathFromRequestedURLParameters = requestedURLParameters.get('path');
+
+          if (pathFromRequestedURLParameters) {
+            await compiler.compile(pathFromRequestedURLParameters);
+
+            response.end();
+
+            return;
+          }
+        }
+
+        if (isCompilerInputFileRequested) {
+          response.end(JSON.stringify(compiler.inputFile.$));
+
+          return;
+        }
+
+        if (isCompilerOutputFileRequested) {
+          response.end(JSON.stringify(compiler.outputFile.$));
+
+          return;
+        }
+
+        // Statistics
+
+        const isStatisticsFileRequested = requestedURL.pathname === `/${statisticsFile.fileName}`;
+
+        if (isStatisticsFileRequested) {
+          const urlFromRequestedUrlParameters = requestedURLParameters.get('url');
+
+          if (request.headers.referer && request.headers['user-agent'] && urlFromRequestedUrlParameters) {
+            statisticsFile.$.requests = [
+              {
+                headers: [
+                  [ 'referer', request.headers.referer, ],
+                  [ 'user-agent', request.headers['user-agent'], ],
+                ],
+                url: new URL(urlFromRequestedUrlParameters).toString(),
+              },
+              ...statisticsFile.$.requests,
+            ];
+
+            statisticsFile.writeFile();
+          }
+
+          response.end(JSON.stringify(statisticsFile.$));
+
+          return;
+        }
+
+        const $ = mime(path.extname(requestedURL.pathname));
+
+        response.setHeader('Content-Type', $.charset ? `${$.typeName}; charset=${$.charset}` : $.typeName);
+
+        response.end(await readFile(`.${requestedURL.pathname}`, 'base64'), 'base64');
+      } catch (error) {
+        response.statusCode = 500;
+
+        response.end(JSON.stringify({ errors: [ error.message, ], }));
       }
+    });
 
-      if (isCompilerInputFileRequested) {
-        response.end(JSON.stringify(compiler.inputFile.$));
+    createdServer.listen(1337);
+  }
 
-        return;
-      }
-
-      if (isCompilerOutputFileRequested) {
-        response.end(JSON.stringify(compiler.outputFile.$));
-
-        return;
-      }
-
-      const $ = mime(path.extname(requestedURL.pathname));
-
-      response.setHeader('Content-Type', $.charset ? `${$.typeName}; charset=${$.charset}` : $.typeName);
-
-      response.end(await readFile(`.${requestedURL.pathname}`, 'base64'), 'base64');
-    } catch (error) {
-      response.statusCode = 500;
-
-      response.end(JSON.stringify({ errors: [ error.message, ], }));
-    }
-  });
-
-  server.listen(1337);
-
-  process.stdout.write(`\x1b[31m    x  x\n x        x\nx          x\nx          x\n x        x\n    x  x\x1b[0m\n\n${p.name}\n${p.version}\n`);
+  createServer('?', '3.0.0-beta.21');
 }
