4af02589e1e2813f6ab281bc1f138ed8e878ceaf
Marek Kobida <marek.kobida@gmail.com>
2020-06-23 23:40:59 +0200

    up

--- private/electron.ts
+++ private/electron.ts
@@ -1,8 +1,12 @@
 import electronPath from './electronPath';
 import path from 'path';
 import server from './server';
-import { Menu, MenuItem, Tray, app, ipcMain, } from 'electron';
+import { BrowserWindow, Menu, MenuItem, Tray, app, ipcMain, } from 'electron';
 
+const WINDOW_HEIGHT = 256;
+const WINDOW_WIDTH = 256;
+
+let browserWindow: BrowserWindow | null = null;
 let menu: Menu | null = null;
 let tray: Tray | null = null;
 
@@ -12,6 +16,23 @@ app.on('ready', () => {
   if (process.platform === 'darwin') {
     app.dock.hide();
   }
+
+  browserWindow = new BrowserWindow({
+    frame: false,
+    height: WINDOW_HEIGHT,
+    resizable: false,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    width: WINDOW_WIDTH,
+  });
+
+  browserWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+
+  browserWindow.on('blur', browserWindow.hide);
+
+  browserWindow.on('close', () => browserWindow = null);
 
   menu = new Menu();
 
