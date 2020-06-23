e011468f5c8f2564a5b12f360b77e36018d9060d
Marek Kobida <marek.kobida@gmail.com>
2020-06-23 14:43:46 +0200

    compiler 3.0.0-beta.25 (electron powered)

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '../redredsk.app/Contents/Resources/app/'),
+    path: path.resolve(__dirname, './electron'),
   },
   resolve: {
     extensions: [
