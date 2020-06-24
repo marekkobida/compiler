1ecc634593278c9c3eea710016fbc76194f77de7
Marek Kobida <marek.kobida@gmail.com>
2020-06-24 11:45:56 +0200

    compiler 3.1.0

--- private/electron.ts
+++ private/electron.ts
@@ -1,12 +1,8 @@
 import electronPath from './electronPath';
 import path from 'path';
 import server from './server';
-import { BrowserWindow, Menu, MenuItem, Tray, app, ipcMain, } from 'electron';
+import { Menu, MenuItem, Tray, app, } from 'electron';
 
-const WINDOW_HEIGHT = 256;
-const WINDOW_WIDTH = 256;
-
-let browserWindow: BrowserWindow | null = null;
 let menu: Menu | null = null;
 let tray: Tray | null = null;
 
@@ -17,23 +13,6 @@ app.on('ready', () => {
     app.dock.hide();
   }
 
-  browserWindow = new BrowserWindow({
-    frame: false,
-    height: WINDOW_HEIGHT,
-    resizable: false,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    width: WINDOW_WIDTH,
-  });
-
-  browserWindow.loadURL(`file://${app.getAppPath()}/index.html`);
-
-  browserWindow.on('blur', browserWindow.hide);
-
-  browserWindow.on('close', () => browserWindow = null);
-
   menu = new Menu();
 
   tray = new Tray(path.join(`${app.getAppPath()}/iconTemplate@2x.png`));
@@ -42,7 +21,5 @@ app.on('ready', () => {
 
   tray.setContextMenu(menu);
 
-  ipcMain.on('startServer', async (event, name) => {
-    server(name).listen(1337);
-  });
+  server('').listen(1337);
 });
